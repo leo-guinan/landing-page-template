7f6bd79bacb3d0e1c0bd4dd49549d0813c0ba917
leo-guinan <leo@copy.ai>
2022-05-08 11:40:37 -0400

    landing page template

---
+++ src/components/SocialProof.tsx
@@ -0,0 +1,10 @@
+// @ts-ignore
+import FamewallEmbed from 'react-famewall'
+
+export default function SocialProof() {
+    return (
+        <FamewallEmbed
+            wallUrl="feathercrm"
+        />
+    )
+}
